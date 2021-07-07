6b1075e45a58f2854f95bc98c0ad381b3ce08f2e
cuixiaoming <cuixm@133.cn>
2021-07-07 17:10:39 +0800

    去掉导包

--- app/src/main/java/com/rookie/travelinandroid/super_ui/animator/ProvinceTypeEvaluator.java
+++ app/src/main/java/com/rookie/travelinandroid/super_ui/animator/ProvinceTypeEvaluator.java
@@ -1,7 +1,6 @@
 package com.rookie.travelinandroid.super_ui.animator;
 
 import android.animation.TypeEvaluator;
-import android.util.Log;
 
 public class ProvinceTypeEvaluator implements TypeEvaluator<String> {
     @Override
@@ -11,7 +10,6 @@ public class ProvinceTypeEvaluator implements TypeEvaluator<String> {
         int endIndex = ProvinceUtil.cityList.indexOf(endValue);
         int index = (int) ((endIndex - startIndex) * fraction + startIndex);
         String s = ProvinceUtil.cityList.get(index);
-        Log.e("test","return:"+s);
         return s;
     }
 }
