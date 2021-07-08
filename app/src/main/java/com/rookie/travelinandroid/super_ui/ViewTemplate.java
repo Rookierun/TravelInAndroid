f75f93700955e8d00b443d32f11c1f2ac9008389
cuixiaoming <cuixm@133.cn>
2021-07-08 11:32:47 +0800

    完善ViewTemplate

--- app/src/main/java/com/rookie/travelinandroid/super_ui/ViewTemplate.java
+++ app/src/main/java/com/rookie/travelinandroid/super_ui/ViewTemplate.java
@@ -1,12 +1,16 @@
 package com.rookie.travelinandroid.super_ui;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class ViewTemplate extends View {
+    Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
     public ViewTemplate(Context context) {
         this(context, null);
     }
@@ -22,5 +26,25 @@ public class ViewTemplate extends View {
 
     private void init() {
 
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
