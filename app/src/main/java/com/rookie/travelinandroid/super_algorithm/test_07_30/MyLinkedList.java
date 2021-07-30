9fe972b55d299d3ccf871040cf1c66408f890f07
cuixiaoming <cuixm@133.cn>
2021-07-30 09:57:34 +0800

    算法&数据结构

---
+++ app/src/main/java/com/rookie/travelinandroid/super_algorithm/test_07_30/MyLinkedList.java
@@ -0,0 +1,14 @@
+package com.rookie.travelinandroid.super_algorithm.test_07_30;
+
+public class MyLinkedList {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
