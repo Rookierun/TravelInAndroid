38858c99b8651adbf310712ad012c9e6d8df9426
cuixiaoming <cuixm@133.cn>
2021-08-04 13:38:49 +0800

    合并两个有序链表

--- app/src/main/java/com/rookie/travelinandroid/super_algorithm/test_08_01/Test_Merge_Two_Sorted_Linked_List.java
+++ app/src/main/java/com/rookie/travelinandroid/super_algorithm/test_08_01/Test_Merge_Two_Sorted_Linked_List.java
@@ -9,4 +9,29 @@ public class Test_Merge_Two_Sorted_Linked_List {
     public static void main(String[] args) {
 
     }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 }
